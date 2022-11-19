245ca2d96d70849dc7fdf0230c7786e475e1f3a7
V8PDL <efr.dima2011@yandex.ru>
2022-11-19 19:21:09 +0300

    Smol refactoring

--- ryan_gosling.py
+++ ryan_gosling.py
@@ -1,8 +1,10 @@
 from PIL import Image, ImageDraw
 from random import random, choice 
 from typing import TypeVar, List
-from os import makedirs, path, system, remove, sep
+from os import makedirs, path, system, listdir, sep
 from json import load
+from imageio.v2 import get_writer, imread
+from time import time
 
 Cell = TypeVar('Cell')
 Matrix = TypeVar('Matrix')
@@ -18,9 +20,15 @@ class Matrix:
     colors_dict:dict = dict()
     size:int
     plot_size:float
+    iterations_count:int
+    snapshots_frequency:int
     step:float
     happiness_cup:int
-    
+    cells_path:str
+    temp_folder:str
+    gif_path:str
+    write_text_logs:bool
+
     def __init__(self, config, do_initialization = True):
         if config is not None:
             self.white_rate = config['white_rate']
@@ -29,6 +37,12 @@ class Matrix:
             self.size = config['array_size']
             self.plot_size = config['plot_size']
             self.happiness_cup = config['happiness_cup']
+            self.iterations_count = config['iterations_count']
+            self.snapshots_frequency = config['snapshots_frequency']
+            self.gif_path = config['gif_path']
+            self.temp_folder = config['temp_folder']
+            self.cells_path = config['cells_path']
+            self.write_text_logs = config['write_text_logs']
             self.step = self.plot_size / self.size
 
         if do_initialization:
@@ -38,7 +52,7 @@ class Matrix:
             self.empty_cells = [cell for cell in self.all_cells if cell.color == "empty"]
 
     class Cell:
-        
+
         matrix:Matrix
         color:str
         position:int
@@ -92,10 +106,9 @@ class Matrix:
                     self.matrix.get_cell_by_xy(self.position_x - 1, self.position_y + 1),
                     self.matrix.get_cell_by_xy(self.position_x - 1, self.position_y),
                     self.matrix.get_cell_by_xy(self.position_x - 1, self.position_y - 1)]
-        
+
         def exchange_places(self) -> bool:
             if len(self.matrix.empty_cells) < 1:
-                print("Too fast...")
                 return False
             empty_cell = choice(self.matrix.empty_cells)
             (self.position, empty_cell.position) = (empty_cell.position, self.position)
@@ -119,54 +132,59 @@ class Matrix:
         new_matrix.empty_cells = self.empty_cells.copy()
         new_matrix.happy_cells = self.happy_cells.copy()
         new_matrix.unhappy_cells = self.unhappy_cells.copy()
-
         return new_matrix
 
-    def get_image(self, temp_folder_path: str = None, counter: int = None) -> Image:
+    def get_image(self, iteration: int = None) -> Image:
         image = Image.new("RGBA", (self.plot_size, self.plot_size), self.colors_dict["empty"])
         draw = ImageDraw.Draw(image)
-        draw.rectangle([0, 0, self.plot_size, self.plot_size], fill=self.colors_dict["empty"])
-        [draw.rectangle([cell.position_x * self.step, cell.position_y * self.step, (cell.position_x + 1) * self.step, (cell.position_y + 1) * self.step], fill=self.colors_dict[cell.color]) for cell in self.all_cells]
-        if temp_folder_path is not None:
-            image.save(f"{temp_folder_path}{sep}{counter}.png", "png", save_all=True)
-            cells_to_text = str.join(" ; ", [f"{cell.position_x}:{cell.position_y} = {cell.color} ({cell.is_happy})" for cell in self.all_cells])
-            with open(f"{temp_folder_path}{sep}cells.txt", "a") as cells_file:
-                cells_file.write(f"Cells on iteration {counter}:\n{cells_to_text}\n")
+        [draw.rectangle(
+            [cell.position_x * self.step, cell.position_y * self.step, (cell.position_x + 1) * self.step, (cell.position_y + 1) * self.step],
+            fill=self.colors_dict[cell.color])
+        for cell in self.all_cells]
+        image.save(f"{self.temp_folder}{sep}{iteration}.png", "png", save_all=True)
         return image
 
-    def iterate(self, iteration: int, iterations_need:int = None, temp_folder:str = None):
-        if temp_folder is None or choice(self.unhappy_cells).exchange_places() == False:
-            return None
-        print()
-        if iterations_need % (2 * iteration) == 0:
-            print(f"{iteration / iterations_need * 100:1.2f}% is done")
-        return self.get_image(temp_folder, iteration)
+    def iterate(self, iteration: int = 1) -> bool:
+        if len(self.unhappy_cells) == 0:
+            return False
+        if self.write_text_logs:
+            cells_to_text = str.join("", [f"{cell.position_x}:{cell.position_y} = {cell.color} ({cell.is_happy})" for cell in self.all_cells])
+            with open(f"{self.temp_folder}{sep}{self.cells_path}", "a") as cells_file:
+                cells_file.write(f"Cells on iteration {iteration}:\n{cells_to_text}\n")
+        if iteration % self.snapshots_frequency == 0:
+            print(f"{iteration / self.iterations_count * 100:1.2f}% is done")
+            self.get_image(iteration)
+        return True
+
+    def make_gif(self):
+        images = [image_path for image_path in listdir(self.temp_folder) if image_path.endswith(".png")]
+        with get_writer(self.gif_path, mode="I", duration=0.1) as writer:
+            [writer.append_data(imread(f"{self.temp_folder}{sep}{image}")) for image in images]
 
 if __name__ == "__main__":
-    
+
     with open("config.json") as config_file:
         config = load(config_file)
-        iterations_count = config['iterations_count']
-        snapshots_frequency = config['snapshots_frequency']
-        temp_folder = config['temp_folder']
-        gif_path = config['gif_path']
-        cells_path = config['cells_path']
+        generate_images = config['generate_images']
+        generate_gif = config['generate_gif']
+
+    if generate_images:
+        print("Start generating images")
+        images_start_time = time()
         matrix = Matrix(config)
-    print(config)
-    [cell.state_happiness() for cell in matrix.all_cells]
-    if path.exists(temp_folder):
-        system(f"rd /s /q \"{temp_folder}\"")
-    makedirs(temp_folder)
-    if path.exists(f"{temp_folder}{sep}{cells_path}"):
-        remove(f"{temp_folder}{sep}{cells_path}")
-    images = list()
-    [images.append(matrix.iterate(iteration, iterations_count, iteration % snapshots_frequency == 0, temp_folder)) for iteration in range(0, iterations_count)]
-    print('100% is done, start converting images to gif')
-    if len(images) > 0:
-        images[0].save(
-            gif_path,
-            save_all = True,
-            append_images = [i for i in images[1:] if i is not None],
-            optimize = True,
-            duration = 100,
-            loop = 0)
+        [cell.state_happiness() for cell in matrix.all_cells]
+        if path.exists(matrix.temp_folder):
+            system(f"rd /s /q \"{matrix.temp_folder}\"")
+        makedirs(matrix.temp_folder)
+        [matrix.iterate(iteration) for iteration in range(0, matrix.iterations_count)]
+        images_end_time = time()
+        print(f"Images generated. It took {images_end_time - images_start_time:1.9f}")
+
+    if generate_gif:
+        print('Start converting images to gif')
+        gif_start_time = time()
+        matrix.make_gif()
+        gif_end_time = time()
+        print(f"Gif created. It took {gif_end_time - gif_start_time:1.9f}")
+
+    print("Job is done.")
