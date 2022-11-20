a15556b77384f1aa748b9499971c58c73739053d
V8PDL <efr.dima2011@yandex.ru>
2022-11-20 13:50:58 +0300

    Oh no, refactoring!

--- ryan_gosling.py
+++ ryan_gosling.py
@@ -28,8 +28,10 @@ class Matrix:
     temp_folder:str
     gif_path:str
     write_text_logs:bool
+    write_diffs:bool
+    seconds_for_frame:float
 
-    def __init__(self, config, do_initialization = True):
+    def __init__(self, config):
         if config is not None:
             self.white_rate = config['white_rate']
             self.black_rate = config['black_rate']
@@ -43,13 +45,14 @@ class Matrix:
             self.temp_folder = config['temp_folder']
             self.cells_path = config['cells_path']
             self.write_text_logs = config['write_text_logs']
+            self.write_diffs = config['write_diffs']
+            self.seconds_for_frame = config['seconds_for_frame']
             self.step = self.plot_size / self.size
 
-        if do_initialization:
-            [self.all_cells.append(self.Cell(self, position)) for position in range(0, self.size * self.size)]
-            self.unhappy_cells = [cell for cell in self.all_cells if not cell.is_happy]
-            self.happy_cells = [cell for cell in self.all_cells if cell.is_happy]
-            self.empty_cells = [cell for cell in self.all_cells if cell.color == "empty"]
+        [self.all_cells.append(self.Cell(self, position)) for position in range(0, self.size * self.size)]
+        self.unhappy_cells = [cell for cell in self.all_cells if not cell.is_happy]
+        self.happy_cells = [cell for cell in self.all_cells if cell.is_happy]
+        self.empty_cells = [cell for cell in self.all_cells if cell.color == "empty"]
 
     class Cell:
 
@@ -87,7 +90,7 @@ class Matrix:
         def state_happiness(self) -> bool:
             if self.color == "empty":
                 return True
-            new_is_happy = sum([self.color == neighbour.color for neighbour in self.get_neighbours()]) > self.matrix.happiness_cup
+            new_is_happy = sum([self.color == neighbour.color for neighbour in self.get_neighbours()]) >= self.matrix.happiness_cup
             if new_is_happy != self.is_happy and self.is_happy:
                 self.matrix.happy_cells.remove(self)
                 self.matrix.unhappy_cells.append(self)
@@ -107,59 +110,66 @@ class Matrix:
                     self.matrix.get_cell_by_xy(self.position_x - 1, self.position_y),
                     self.matrix.get_cell_by_xy(self.position_x - 1, self.position_y - 1)]
 
-        def exchange_places(self) -> bool:
+        def exchange_places(self):
             if len(self.matrix.empty_cells) < 1:
-                return False
+                return
             empty_cell = choice(self.matrix.empty_cells)
-            (self.position, empty_cell.position) = (empty_cell.position, self.position)
-            (self.position_x, empty_cell.position_x) = (empty_cell.position_x, self.position_x)
-            (self.position_y, empty_cell.position_y) = (empty_cell.position_y, self.position_y)
+            if self.matrix.write_diffs:
+                with open(f"{self.matrix.temp_folder}{sep}{self.matrix.cells_path}", "a") as cells_file:
+                    cells_file.write(f"Cell {self.to_string()} moved to {empty_cell.to_string()}. Neighbours:\n")
+                    [cells_file.write(f"\t{cell.to_string()}\n") for cell in self.get_neighbours()]
+            (self.color, self.is_happy, empty_cell.color) = (empty_cell.color, True, self.color)
+            self.matrix.empty_cells.remove(empty_cell)
+            self.matrix.empty_cells.append(self)
+            self.matrix.happy_cells.append(self)
+            self.matrix.unhappy_cells.remove(self)
             [neighbour.state_happiness() for neighbour in self.get_neighbours()]
-            self.state_happiness()
+            empty_cell.state_happiness()
             [neighbour.state_happiness() for neighbour in empty_cell.get_neighbours()]
-            return True
+
+        def to_string(self) -> str:
+            return f"[{self.position}]{self.position_x}:{self.position_y} = {self.color} ({self.is_happy})"
 
     def get_cell_by_xy(self, x: int, y: int) -> Cell:
-        if x >= self.size or y >= self.size:
-            # Empty cell
-            return self.Cell(self, 0, 1)
-        else:
-            return self.all_cells[x * self.size + y]
+        if x < 0 or y < 0 or x >= self.size or y >= self.size:
+            return self.Cell(matrix, 0, 1)
+        return self.all_cells[x * self.size + y]
 
-    def copy(self) -> Matrix:
-        new_matrix = Matrix(False)
-        new_matrix.all_cells = self.all_cells.copy()
-        new_matrix.empty_cells = self.empty_cells.copy()
-        new_matrix.happy_cells = self.happy_cells.copy()
-        new_matrix.unhappy_cells = self.unhappy_cells.copy()
-        return new_matrix
-
-    def get_image(self, iteration: int = None) -> Image:
+    def get_image(self) -> Image:
         image = Image.new("RGBA", (self.plot_size, self.plot_size), self.colors_dict["empty"])
         draw = ImageDraw.Draw(image)
         [draw.rectangle(
             [cell.position_x * self.step, cell.position_y * self.step, (cell.position_x + 1) * self.step, (cell.position_y + 1) * self.step],
             fill=self.colors_dict[cell.color])
         for cell in self.all_cells]
-        image.save(f"{self.temp_folder}{sep}{iteration}.png", "png", save_all=True)
         return image
 
-    def iterate(self, iteration: int = 1) -> bool:
+    def save_image(self, iteration:int = 0):
+        self.get_image().save(f"{self.temp_folder}{sep}{iteration}.png", "png", save_all=True)
+
+    def iterate(self, iteration: int = 0) -> bool:
         if len(self.unhappy_cells) == 0:
+            print("Everyone is happy")
+            self.save_image(iteration)
             return False
-        if self.write_text_logs:
-            cells_to_text = str.join("", [f"{cell.position_x}:{cell.position_y} = {cell.color} ({cell.is_happy})" for cell in self.all_cells])
-            with open(f"{self.temp_folder}{sep}{self.cells_path}", "a") as cells_file:
-                cells_file.write(f"Cells on iteration {iteration}:\n{cells_to_text}\n")
+        choice(self.unhappy_cells).exchange_places()
         if iteration % self.snapshots_frequency == 0:
-            print(f"{iteration / self.iterations_count * 100:1.2f}% is done")
-            self.get_image(iteration)
+            if iteration % int(self.iterations_count / 100) == 0:
+                print(f"{int(iteration / self.iterations_count * 100)}% is done")
+            self.save_image(iteration)
+            if self.write_text_logs:
+                cells_to_text = str.join(";", [cell.to_string() for cell in self.all_cells])
+                with open(f"{self.temp_folder}{sep}{self.cells_path}", "a") as cells_file:
+                    cells_file.write(f"\nMatrix on iteration {iteration}:\n{cells_to_text}\n")
         return True
 
     def make_gif(self):
-        images = [image_path for image_path in listdir(self.temp_folder) if image_path.endswith(".png")]
-        with get_writer(self.gif_path, mode="I", duration=0.1) as writer:
+        images = sorted([image_path for image_path in listdir(self.temp_folder) if image_path.endswith(".png")], key=lambda i: int(i[:-4]))
+        with get_writer(self.gif_path, mode="I", duration=self.seconds_for_frame) as writer:
             [writer.append_data(imread(f"{self.temp_folder}{sep}{image}")) for image in images]
+
+    def to_string(self) -> str:
+        return str.join(";", [cell.to_string() for cell in self.all_cells])
 
 if __name__ == "__main__":
 
@@ -176,7 +186,12 @@ if __name__ == "__main__":
         if path.exists(matrix.temp_folder):
             system(f"rd /s /q \"{matrix.temp_folder}\"")
         makedirs(matrix.temp_folder)
-        [matrix.iterate(iteration) for iteration in range(0, matrix.iterations_count)]
+        if matrix.write_text_logs:
+            with open(f"{matrix.temp_folder}{sep}{matrix.cells_path}", "a") as cells_file:
+                cells_file.write(f"Starting matrix:\n{matrix.to_string()}\n")
+        for iteration in range(0, matrix.iterations_count):
+            if not matrix.iterate(iteration):
+                break
         images_end_time = time()
         print(f"Images generated. It took {images_end_time - images_start_time:1.9f}")
 
